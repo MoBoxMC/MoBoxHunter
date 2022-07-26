398896b0a114a3c5b32451aa0f50620273e169da
MossCG <77914339+MossCG@users.noreply.github.com>
2022-07-27 00:37:04 +0800

    V1.5.2.1.0029 重写角色分配机制，优化分配代码

--- src/main/java/org/mossmc/mosscg/MoBoxHunter/Player/PlayerDistribute.java
+++ src/main/java/org/mossmc/mosscg/MoBoxHunter/Player/PlayerDistribute.java
@@ -7,45 +7,56 @@ import org.bukkit.entity.Player;
 import org.mossmc.mosscg.MoBoxHunter.BasicInfo;
 import org.mossmc.mosscg.MoBoxHunter.Main;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.UUID;
 
 public class PlayerDistribute {
     public static void distribute() {
-        //初始化数据，这里的数据都是不会直接改变的
-        int count = PlayerCache.playerList.size();//玩家总数
-        int maxRunner = Main.getConfig.getInt("maxRunner");//最大逃亡者数
-        int maxPlayer = Main.getConfig.getInt("maxPlayer");//最大玩家数
-        int step = maxPlayer/maxRunner;//每增加一个逃亡者所需要的人数
-        //计算逃亡者数量
-        int runner = 1;//逃亡者数
-        int countCache = count;
-        while (countCache > step) {
-            countCache = countCache - step;
-            runner++;
-        }
-        //随机分配逃亡者
-        Random random = new Random();
-        int runnerCache = runner;
-        while (runnerCache > 0) {
-            int target = random.nextInt(count-1);
-            OfflinePlayer player = Bukkit.getOfflinePlayer(PlayerCache.playerList.get(target));
-            if (PlayerCache.runnerList.contains(PlayerCache.playerList.get(target))) {
-                runnerCache--;
-                continue;
-            }
-            PlayerCache.runnerList.add(PlayerCache.playerList.get(target));
-            PlayerCache.runnerStatusMap.put(PlayerCache.playerList.get(target), BasicInfo.runnerStatus.Alive);
-            Main.logger.info(ChatColor.GREEN+"玩家"+player.getName()+"已被分配到逃亡者");
-            runnerCache--;
-        }
-        //剩下为猎人
-        PlayerCache.playerList.forEach(target -> {
-            OfflinePlayer player = Bukkit.getOfflinePlayer(target);
-            if (!PlayerCache.runnerList.contains(target)) {
-                PlayerCache.hunterList.add(target);
-                Main.logger.info(ChatColor.GREEN+"玩家"+player.getName()+"已被分配到猎杀者");
+        //除重，cacheList后续会用到
+        List<UUID> cacheList = new ArrayList<>();
+        PlayerCache.playerList.forEach(uuid -> {
+            if (!cacheList.contains(uuid)) {
+                cacheList.add(uuid);
             }
         });
-        //自此，分配完成
+        PlayerCache.playerList.clear();
+        PlayerCache.playerList = new ArrayList<>(cacheList);
+
+        //初始化数据，这里的数据都是不会直接改变的
+        int count = PlayerCache.playerList.size();//玩家总数
+        int partRunner = Main.getConfig.getInt("partRunner");//逃亡者比例
+        int partHunter = Main.getConfig.getInt("partHunter");//猎杀者比例
+
+        //初始化变量，这里都是会动的变量
+        int remain = count;
+        int remainRunner = partRunner;
+        int remainHunter = partHunter;
+        Random random = new Random();
+
+        //分配
+        while (remain > 0) {
+            remain--;
+            UUID playerUUID = cacheList.get(random.nextInt(cacheList.size()));
+            OfflinePlayer player = Bukkit.getOfflinePlayer(playerUUID);
+            cacheList.remove(playerUUID);
+            if (remainRunner <= 0 && remainHunter <= 0) {
+                remainRunner = partRunner;
+                remainHunter = partHunter;
+            }
+            if (remainRunner > 0) {
+                PlayerCache.runnerList.add(playerUUID);
+                PlayerCache.runnerStatusMap.put(playerUUID, BasicInfo.runnerStatus.Alive);
+                Main.logger.info(ChatColor.GREEN+"玩家"+player.getName()+"已被分配到逃亡者");
+                remainRunner--;
+                continue;
+            }
+            if (remainHunter > 0) {
+                PlayerCache.hunterList.add(playerUUID);
+                Main.logger.info(ChatColor.GREEN+"玩家"+player.getName()+"已被分配到猎杀者");
+                remainHunter--;
+            }
+        }
     }
 }
